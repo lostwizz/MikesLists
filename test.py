25ee29bed02efecc0485c5cc43b83008a5335f1e
lostwizz@hotmail.com <lostwizz@hotmail.com>
2026-01-08 00:21:08 -0500

    more

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+r"""
+test.py
+
+
+"""
+__version__ = "0.0.0.000060-dev"
+__author__ = "Mike Merrett"
+__updated__ = "2026-01-03 22:25:49"
+###############################################################################
+
+from .core import *
+
+#EXTRA_ALLOWED_HOSTS += []
+
+TEMPLATES[0]['OPTIONS']['context_processors'].append(
+    'MikesLists.context_processors.env_name'
+)
