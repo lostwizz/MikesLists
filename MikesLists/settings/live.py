b44057a05f89ddc1e7ce9782566bfe6f3ae2f15b
lostwizz@hotmail.com <lostwizz@hotmail.com>
2026-01-08 00:27:20 -0500

    more

--- MikesLists/settings/live.py
+++ MikesLists/settings/live.py
@@ -6,7 +6,7 @@ live.py
 
 
 """
-__version__ = "0.0.0.000007-dev"
+__version__ = "0.0.0.000070-dev"
 __author__ = "Mike Merrett"
 __updated__ = "2026-01-03 22:44:35"
 ###############################################################################
@@ -14,7 +14,7 @@ __updated__ = "2026-01-03 22:44:35"
 
 from .core import *
 
-EXTRA_ALLOWED_HOSTS += []
+# EXTRA_ALLOWED_HOSTS += []
 
 TEMPLATES[0]['OPTIONS']['context_processors'].append(
     'MikesLists.context_processors.env_name'
--- MikesLists/settings/test.py
+++ MikesLists/settings/test.py
@@ -5,26 +5,15 @@ r"""
 test.py
 
 
-# TODO:
-# COMMENT:
-# NOTE:
-# USEFULL:
-# LEARN:
-# RECHECK:
-# INCOMPLETE:
-# SEE NOTES:
-# POST
-# HACK
-
 """
-__version__ = "0.0.0.000007-dev"
+__version__ = "0.0.0.000060-dev"
 __author__ = "Mike Merrett"
 __updated__ = "2026-01-03 22:25:49"
 ###############################################################################
 
 from .core import *
 
-EXTRA_ALLOWED_HOSTS += []
+#EXTRA_ALLOWED_HOSTS += []
 
 TEMPLATES[0]['OPTIONS']['context_processors'].append(
     'MikesLists.context_processors.env_name'
